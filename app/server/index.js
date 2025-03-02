5a998febdb2e644e4d6280337f041831a005d977
yoonseoc126 <yoonseoc@usc.edu>
2025-03-01 16:21:34 -0800

    Add gemini expiration query

--- app/server/index.js
+++ app/server/index.js
@@ -4,6 +4,23 @@ const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI("AIzaSyAtoiqA2MQ1XJy4KqnC_KLLWGaf9PMcUWk");
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+
+async function getExpirationDays(item){
+  const prompt = "Act as a expiration date expert and tell me how many days on minimum a " + item + " is good for consumption from its purchase date in the fridge. Only give me one number. Example: 10";
+  
+  try {
+    const result = await model.generateContent(prompt);
+    console.log("Response:", result.response.text());
+    return result.response.text(); // Return the response text
+  } catch (error) {
+    console.error("Error generating content:", error);
+    return "An error occurred while fetching the answer.";
+  }
+}
 
 // Middleware
 app.use(cors());
@@ -13,6 +30,12 @@ app.use(express.json()); // For JSON parsing
 app.get('/', (req, res) => {
   res.send('Hello Express server is running!');
 });
+
+app.get('/rot', async (req, res) =>{
+  const answer = await getExpirationDays("banana");
+
+  res.send(answer);
+})
 
 // Start server
 app.listen(PORT, () => {
